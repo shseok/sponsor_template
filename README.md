081604fc9ef72dcdf13843d25a36d2126220f30c
shseok <gus0674@ajou.ac.kr>
2022-01-13 14:58:42 +0900

    Add carousel effect

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 
 # Caution
 - google map을 사용하기 위해 api key가 필요
-- type = submit
+- type = submit or modal (in business_register)
--- main/js/scripts.js
+++ main/js/scripts.js
@@ -78,3 +78,43 @@ function fadeIn(el, display) {
         }
     })();
 };
+
+let minutes = document.getElementsByClassName('minute')
+
+function wipeIn(element) {
+    element.classList.add('wipe-in')
+}
+
+function wipeOut(element) {
+    element.classList.add('wipe-out')
+    delay(1700).then(() => reset(element))
+}
+
+function reset(element) {
+    element.classList = 'minute'
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function* carousel(elements) {
+    for (let i = 0; i >= 0; i++) {
+        if (i === 0) {
+            wipeIn(elements[i])
+            yield
+        }
+
+        wipeOut(elements[i % elements.length])
+        wipeIn(elements[(i + 1) % elements.length])
+        yield
+    }
+}
+
+function runCarousel(genObj) {
+    if (!genObj.next().done) {
+        setTimeout(runCarousel, 2000, genObj)
+    }
+}
+
+runCarousel(carousel(minutes))
